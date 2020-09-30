e7d52039fb0d17a42fa6a1bea6fcb6268e51e57e
pratikgp <pratik.girish.patel@gmail.com>
2020-09-30 11:14:09 -0500

    Flix Part 2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Pratik Patel on 9/29/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
